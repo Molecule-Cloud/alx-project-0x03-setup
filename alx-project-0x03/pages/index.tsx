e5da86be5ccb5644cbd75a7cb73f1c69a5839b21
Benjamin Appiah <ben.appiah451@gmail.com>
2025-07-27 19:35:00 +0000

    Added 404 Page

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,7 +1,7 @@
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
 import Layout from "@/components/layouts/Layout"
-import { PageRouteProps } from '@/interfac'
+import { PageRouteProps } from '@/interface'
 
 
 
@@ -15,7 +15,6 @@ export default function Home() {
 
   return (
     <>
-    <Layout />
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
       {/* Welcome Message */}
       <h1 className="text-4xl font-bold text-gray-800 mb-4">
